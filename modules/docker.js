539da1126ed0cfb215830232f89f97f34c518d32
huanghuanlai <102535481@qq.com>
2017-09-01 14:23:49 +0800

    limit range of port

--- modules/docker.js
+++ modules/docker.js
@@ -11,25 +11,43 @@ module.exports = function () {
             return service.exist(ctx)
         }).post('/docker/create', async = ctx => {
             var containerName = ctx.request.body.containerName
-            if(containerName==''){
+            var projectPort = ctx.request.body.projectPort | 0
+            if (containerName == '') {
                 ctx.body = {
-                    code:1,
-                    msg:'containerName不能为空'
+                    code: 1,
+                    msg: 'containerName不能为空'
                 }
-            }else{
+            } else {
                 var cns = containerName.split('-')[1]
-                if(containerName.indexOf('-')==-1){
+                if (containerName.indexOf('-') == -1) {
                     ctx.body = {
-                        code:1,
-                        msg:'containerName非法'
+                        code: 1,
+                        msg: 'containerName非法'
                     }
-                }else if(cns!='node'&&cns!='consumer'&&cns!='provider'){
+                } else if (cns != 'node' && cns != 'consumer' && cns != 'provider') {
                     ctx.body = {
-                        code:1,
-                        msg:'containerName非法,只能为-node,-consumer,-provider结尾'
+                        code: 1,
+                        msg: 'containerName非法,只能为-node,-consumer,-provider结尾'
                     }
-                }else{
-                    return service.create(ctx)
+                } else {
+                    if (cns == 'node' && (projectPort < 5000 || projectPort >= 5200)) {
+                        ctx.body = {
+                            code: 1,
+                            msg: 'node端口范围5000 <= port < 5200'
+                        }
+                    } else if (cns == 'consumer' && (projectPort < 5200 || projectPort >= 5400)) {
+                        ctx.body = {
+                            code: 1,
+                            msg: 'consumer端口范围5200 <= port < 5400'
+                        }
+                    } else if (cns == 'provider' && (projectPort < 5400 || projectPort >= 5600)) {
+                        ctx.body = {
+                            code: 1,
+                            msg: 'provider端口范围5400 <= port < 5600'
+                        }
+                    } else {
+                        return service.create(ctx)
+                    }
                 }
             }
 
