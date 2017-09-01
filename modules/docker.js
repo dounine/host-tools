b8d0059bd32aff69978f29ca8963aa5be4b4a318
huanghuanlai <102535481@qq.com>
2017-09-01 14:07:24 +0800

    update

--- modules/docker.js
+++ modules/docker.js
@@ -10,7 +10,29 @@ module.exports = function () {
         }).post('/docker/exist/:name', async = ctx => {
             return service.exist(ctx)
         }).post('/docker/create', async = ctx => {
-            return service.create(ctx)
+            var containerName = ctx.request.body.containerName
+            if(containerName==''){
+                ctx.body = {
+                    code:1,
+                    msg:'containerName不能为空'
+                }
+            }else{
+                var cns = containerName.indexOf('-')[1]
+                if(containerName.indexOf('-')==-1){
+                    ctx.body = {
+                        code:1,
+                        msg:'containerName非法'
+                    }
+                }else if(cns!='node'&&cns!='consumer'&&cns!='provider'){
+                    ctx.body = {
+                        code:1,
+                        msg:'containerName非法,只能为-node,-consumer,-provider结尾'
+                    }
+                }else{
+                    return service.create(ctx)
+                }
+            }
+
         }).post('/docker/del/:name', async = ctx => {
             return service.del(ctx)
         }).post('/docker/stop/:name', async = ctx => {
