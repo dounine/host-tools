3ab7f4a896c2b846fb49e1b59f39458def8034ba
huanghuanlai <102535481@qq.com>
2017-09-02 11:23:31 +0800

    update

--- modules/host.js
+++ modules/host.js
@@ -4,6 +4,7 @@ const service = require('./host/service')()
 module.exports = function () {
 
     return new Router()
-        .get('/host/free',async = ctx => {
-	        return service.free(ctx)
-	    })
+        .get('/host/free', async = ctx => {
+            return service.free(ctx)
+        })
+}
